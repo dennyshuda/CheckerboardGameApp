74b40234f1ea0480881c5b2ba312bc9651c82ea4
Denny Setyawan Huda <denycam9@gmail.com>
2026-04-23 22:25:12 +0700

    fix: update OpenAPI document path to v1.json

--- server/Program.cs
+++ server/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwaggerUi(options =>
 {
-    options.DocumentPath = "/openapi/checkerboard.json";
+    options.DocumentPath = "/openapi/v1.json";
 });
 }
 app.UseRouting();
